0d1065e18e61bceeaa9379030f22ae8f4f83667a
mdunggggg <dungmanh74@gmail.com>
2023-07-09 14:59:02 +0700

    Update In ra danh sach doi tuong

---
+++ src/main/kotlin/model/Manager.kt
@@ -0,0 +1,69 @@
+package model
+
+import data.AllDataForExam
+
+object Manager {
+    private var listClient = mutableListOf<Client>()
+    private var listRoom = mutableListOf<Room>()
+    private var listBooking = mutableListOf<Booking>()
+    private var listService = mutableListOf<Service>()
+    enum class ClientSort{
+        ID_CARD, NAME, ADDRESS, PHONE_NUMBER, EMAIL
+    }
+    enum class RoomSort{
+        ID, ROOM_TYPE, PRICE, ROOM_NUMBER
+    }
+    enum class ServiceSort{
+        ID, NAME, PRICE
+    }
+    init {
+        listClient = AllDataForExam.clientList.toMutableList()
+        listRoom = AllDataForExam.roomList.toMutableList()
+        listService = AllDataForExam.serviceList.toMutableList()
+    }
+    fun printListClient() {
+        println("Danh sách khách hàng")
+        listClient.forEach {
+            println(it)
+        }
+    }
+    fun printListRoom() {
+        println("Danh sách phòng")
+        listRoom.forEach {
+            println(it)
+        }
+    }
+     fun printListService() {
+        println("Danh sách dịch vụ")
+        listService.forEach {
+            println(it)
+        }
+    }
+
+    fun sortClient(sort: ClientSort){
+        when(sort){
+            ClientSort.ID_CARD -> listClient.sortBy { it.idCard }
+            ClientSort.NAME -> listClient.sortBy { it.name }
+            ClientSort.ADDRESS -> listClient.sortBy { it.address }
+            ClientSort.PHONE_NUMBER -> listClient.sortBy { it.phoneNumber }
+            ClientSort.EMAIL -> listClient.sortBy { it.email }
+        }
+    }
+    fun sortRoom(sort : RoomSort){
+        when(sort){
+            RoomSort.ID -> listRoom.sortBy { it.id }
+            RoomSort.ROOM_TYPE -> listRoom.sortBy { it.roomType }
+            RoomSort.PRICE -> listRoom.sortBy { it.price }
+            RoomSort.ROOM_NUMBER -> listRoom.sortBy { it.roomNumber }
+        }
+    }
+    fun sortService(sort : ServiceSort){
+        when(sort){
+            ServiceSort.ID -> listService.sortBy { it.id }
+            ServiceSort.NAME -> listService.sortBy { it.name }
+            ServiceSort.PRICE -> listService.sortBy { it.price }
+        }
+    }
+
+
+}
--- src/main/kotlin/view/Menu.kt
+++ src/main/kotlin/view/Menu.kt
@@ -1,5 +1,6 @@
 package view
 
+import model.Manager
 import java.util.*
 
 
@@ -15,6 +16,57 @@ import java.util.*
 
 class Menu {
     private val scanner = Scanner(System.`in`)
+    private fun printList(){
+        println("\t a. Danh sách khách hàng")
+        println("\t b. Danh sách phòng")
+        println("\t c. Danh sách dịch vụ")
+        when(readlnOrNull()) {
+            "a" -> {
+                Manager.printListClient()
+                sortClient()
+            }
+            "b" -> {
+                Manager.printListRoom()
+                sortRoom()
+            }
+            "c" -> {
+                Manager.printListService()
+                sortService()
+            }
+            else -> println("Nhập sai")
+        }
+    }
+    private fun sortClient(){
+        println("Sắp xếp danh sách theo: \t1.ID\t2.Name\t3.Address\t4.Phone\t5.Email")
+        when(readlnOrNull()){
+            "1" -> Manager.sortClient(Manager.ClientSort.ID_CARD)
+            "2" -> Manager.sortClient(Manager.ClientSort.NAME)
+            "3" -> Manager.sortClient(Manager.ClientSort.ADDRESS)
+            "4" -> Manager.sortClient(Manager.ClientSort.PHONE_NUMBER)
+            "5" -> Manager.sortClient(Manager.ClientSort.EMAIL)
+            else -> println("Nhập sai")
+        }
+        Manager.printListClient()
+    }
+    private fun sortRoom(){
+        println("Sắp xếp danh sách theo: \t1.ID\t2.Room Type\t3.Price\t4.Room Number")
+        when(readlnOrNull()){
+            "1" -> Manager.sortRoom(Manager.RoomSort.ID)
+            "2" -> Manager.sortRoom(Manager.RoomSort.ROOM_TYPE)
+            "3" -> Manager.sortRoom(Manager.RoomSort.PRICE)
+            "4" -> Manager.sortRoom(Manager.RoomSort.ROOM_NUMBER)
+            else -> println("Nhập sai")
+        }
+    }
+    private fun sortService(){
+        println("Sắp xếp danh sách theo: \t1.ID\t2.Name\t3.Price")
+        when(readlnOrNull()){
+            "1" -> Manager.sortService(Manager.ServiceSort.ID)
+            "2" -> Manager.sortService(Manager.ServiceSort.NAME)
+            "3" -> Manager.sortService(Manager.ServiceSort.PRICE)
+            else -> println("Nhập sai")
+        }
+    }
     fun run() {
         while (true) {
             println("Menu")
@@ -23,8 +75,7 @@ class Menu {
             println("3. Đặt phòng")
             when (scanner.nextInt()) {
                 1 -> {
-//                    TODO: print list object
-
+                    printList()
                 }
 
                 2 -> {
